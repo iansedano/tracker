ed00bb3aad1c958c06abfe5ac1c5eb44b287bb92
ian <iansedano@gmail.com>
2021-04-16 16:14:32 +0200

    organizing into files

--- Server/html.js
+++ Server/html.js
@@ -4,7 +4,7 @@ function include(filename) {
 
 function doGet() {
   return HtmlService
-    .createTemplateFromFile('index')
+    .createTemplateFromFile('App/index')
     .evaluate()
     .addMetaTag('viewport', 'width=device-width, initial-scale=1');
 }
