b463b521a28ea7be75d9660d4db7cc7b1df093aa
ian <iansedano@gmail.com>
2021-04-21 19:54:33 +0200

    continuing refactor, rendering, updates broken

--- DB/SheetDB.js
+++ DB/SheetDB.js
@@ -77,28 +77,3 @@ class SheetDB {
     }
     
 }
-
-function getData(){
-
-    const file    = SpreadsheetApp.getActive()
-    const sheet   = file.getSheetByName("Tracking")
-    const range   = sheet.getDataRange()
-    const values  = range.getValues()
-
-    return {
-        file    : file,
-        sheet   : sheet,
-        range   : range,
-        values  : values
-    }
-}
-
-function addRowToSheet(row){
-    const file  = SpreadsheetApp.getActive()
-    const sheet = file.getSheetByName("Tracking")
-    sheet.appendRow(row)
-}
-
-function haveEntryForToday() {
-    const { values } = getData()
-}
--- Server/update-reciever.js
+++ Server/update-reciever.js
@@ -3,45 +3,17 @@ function update(payload) {
     //     name: payload.nasme,
     //     value: result,
     //     index: payload.index,
-    //     colIndex: data.fields[payload.name].index
+    //     colIndex: data.fields[payload.name].index,
+    //     row-number: rowNumber
     // }
     console.log("payload recieved", payload)
     Logger.log(payload)
-    
-    const { sheet, values } = getData();
 
-    const headers = values.shift()
-    const headerMetadata = values.shift()
+    db = new SheetDB("Tracking")
 
-    const {year, month, day} = getIndexComponentsFromIndex(payload.index)
+    db.update(payload.rowNumber, payload.colIndex, payload.value)
 
-    console.log(year, month, day)
-
-    try {
-    values.forEach((row, i) => {
-        const rowYear = row[0]
-        const rowMonth = row[1]
-        const rowDay = row[2]
-
-        console.log(rowYear,rowMonth,rowDay)
-
-        if (
-            rowYear == year
-            && rowMonth == month
-            && rowDay == day
-        ) {
-            sheet
-                .getRange(i + 3, payload.colIndex + 1)
-                .setValue(`${payload.value}`)
-            throw "found!"
-        }
-    })
-    } catch (e){
-        if (e === "found!") return payload.value
-        throw e
-    }
-
-    return 1
+    return payload.value
 }
 
 
